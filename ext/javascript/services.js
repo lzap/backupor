5c4bc39e3d4cbd7d53a4700cdb8cf5de8c3db633
Lukas Zapletal <lzap+git@redhat.com>
2011-06-29 23:22:11 +0200

    new provider - any url

--- ext/javascript/services.js
+++ ext/javascript/services.js
@@ -2,6 +2,7 @@ var available_services = [];
 
 available_services.push({
   name: "Test",
+  description: "Test service that only tests internet connection",
   method: "GET",
   params: [],
   url: function (param) {
@@ -10,7 +11,18 @@ available_services.push({
 });
 
 available_services.push({
+  name: "Any URL",
+  description: "Provide your own url that will be downloaded (e.g. http://myserver.com/file.zip)",
+  method: "GET",
+  params: [],
+  url: function (params) {
+    return params[0];
+  }
+});
+
+available_services.push({
   name: "Blogger",
+  description: "Complete backup of Google Blogger",
   method: "GET",
   params: ["Feed id"],
   url: function (params) {
